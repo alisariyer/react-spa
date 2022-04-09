b0e1ab1ba35f2c08e131300802b8e826e1481ade
Ali Sariyer <alisariyer@gmail.com>
2022-04-09 15:09:32 +0000

    Create Home component and user state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
+import React, { useState } from "react";
+import Home from "./Home";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
-    <div className="container text-center">
-      <div className="row justify-content-center">
-        <div className="col-10 col-md-10 col-lg-8 col-xl-7">
-          <div className="display-4 text-primary mt-3 mb-2">
-            Meeting Log
-          </div>
-          <p className="lead">
-            This simple app creates meetings, allows people to check in, and picks random users to award giveaways. It's a good example of a Single Page Application which includes connection to a database and routing. It's a practical way to learn <a href="https://firebase.google.com">Firebase</a>.
-          </p>
-          <a href="/register" className="btn btn-outline-primary me-2">
-            Register
-          </a>
-          <a href="/login" className="btn btn-outline-primary me-2">
-            Login
-          </a>
-          <a href="/meeting" className="btn btn-primary">
-            Meetings
-          </a>
-        </div> {/* columns */}
-      </div>
-    </div>
+    <Home user={user}/>
   );
 }
 
