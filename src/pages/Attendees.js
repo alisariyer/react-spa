9635c253d517a6e4a47659d45b2bb207db668312
Ali Sariyer <alisariyer@gmail.com>
2022-04-11 22:27:34 +0000

    Add search attendees and choose random attendee feature

--- src/pages/Attendees.js
+++ src/pages/Attendees.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { onValue, ref } from "firebase/database";
 import { db } from "../Firebase";
+import { FaUndo, FaRandom } from "react-icons/fa";
 import AttendeesList from "../components/AttendeesList";
 
 export default function Attendees({ adminUser }) {
 
     const { userId, meetingId } = useParams();
     const [displayAttendees, setDisplayAttendees] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
         const attendeesRef = ref(db, `meetings/${userId}/${meetingId}/attendees`) 
@@ -26,6 +28,21 @@ export default function Attendees({ adminUser }) {
         })
     }, []);
 
+    const dataFilter = item => item.attendeeName.toLowerCase().includes(searchQuery.toLowerCase());
+    const filteredAttendees = displayAttendees.filter(dataFilter);
+    function handleChange(e) {
+        setSearchQuery(e.target.value);
+    }
+
+    function resetQuery() {
+        setSearchQuery('');
+    }
+
+    function chooseRandom() {
+        const randomNumber = Math.floor(Math.random() * displayAttendees.length);
+        setSearchQuery(displayAttendees[randomNumber].attendeeName)
+    }
+
     return (
         <div className="container mt-4">
             <div className="row justify-content-center">
@@ -33,10 +50,36 @@ export default function Attendees({ adminUser }) {
                     <h1 className="fw-light text-center">
                         Attendees
                     </h1>
+                    <div className="card bg-light mb-4">
+                        <div className="card-body text-center">
+                            <div className="input-group input-group-lg">
+                            <input 
+                                type="text" 
+                                name="search" 
+                                value={searchQuery}
+                                placeholder="Search Attendees"
+                                className="form-control"
+                                onChange={handleChange}
+                            />
+                            <button
+                                type="button"
+                                className="btn btn-outline-info btn-sm"
+                                title="Choose a random attendee"
+                                onClick={chooseRandom}
+                            ><FaRandom/></button>
+                            <button
+                                type="button"
+                                className="btn btn-outline-info btn-sm"
+                                title="Reset search"
+                                onClick={resetQuery}
+                            ><FaUndo/></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <AttendeesList
-                attendees={displayAttendees}
+                attendees={filteredAttendees}
                 userId={userId}
                 adminUser={adminUser}
                 meetingId={meetingId}
